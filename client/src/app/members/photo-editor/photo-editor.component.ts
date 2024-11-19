d9a55f28ac5092f15cf111f5464c6f4ce7bd5023
Martin Valdez <mgevaldez@hotmail.com>
2024-11-19 11:19:03 -0600

    End of section 11

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,91 @@
+import {Component, inject, input, OnInit, output} from '@angular/core';
+import {Member} from "../../_models/member";
+import {DecimalPipe, NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
+import {FileUploader, FileUploadModule} from "ng2-file-upload";
+import {AccountService} from "../../_services/account.service";
+import {environment} from "../../../environments/environment";
+import {MembersService} from "../../_services/members.service";
+import {Photo} from "../../_models/photo";
+
+@Component({
+  selector: 'app-photo-editor',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf,
+    NgStyle,
+    FileUploadModule,
+    NgClass,
+    DecimalPipe
+  ],
+  templateUrl: './photo-editor.component.html',
+  styleUrl: './photo-editor.component.css'
+})
+export class PhotoEditorComponent implements OnInit {
+  private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+  member = input.required<Member>();
+  uploader?: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  memberChanged = output<Member>();
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  deletePhoto(photo: Photo) {
+    this.memberService.deletePhoto(photo).subscribe({
+      next: _ => {
+        const updatedMember = {...this.member()};
+        updatedMember.photos = updatedMember.photos.filter(p => p.id !== photo.id);
+        this.memberChanged.emit(updatedMember);
+      }
+    })
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: _ => {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user);
+        }
+        const updatedMember = {...this.member()};
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          p.isMain = p.id === photo.id;
+        });
+        this.memberChanged.emit(updatedMember);
+      }
+    })
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.accountService.currentUser()?.token,
+      isHTML5: true,
+      removeAfterUpload: true,
+      autoUpload: false,
+      allowedFileType: ['image'],
+      maxFileSize: 10000000,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      const photo = JSON.parse(response);
+      const updatedMember = {...this.member()};
+      updatedMember.photos.push(photo);
+      this.memberChanged.emit(updatedMember);
+    }
+  }
+}
